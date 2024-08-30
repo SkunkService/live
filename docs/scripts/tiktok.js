3dd6d21eb6547345a17148ce3739238a98510545
SkunkService <skunkplatformer@gmail.com>
2024-08-30 17:51:15 -0300

    Update tiktok.js

--- docs/scripts/tiktok.js
+++ docs/scripts/tiktok.js
@@ -1,11 +1,1 @@
-const tiktok = new TikTokSDK({
-  apiKey: 'awmfvaljwenozzuz'
-});
-
-tiktok.live.list()
-  .then(streams => {
-    console.log('Live streams:', streams);
-  })
-  .catch(error => {
-    console.error('Error fetching live streams:', error);
-  });
+console.log("[WARN]: Could not be working for Stream List!\n[WARN]: Will not be longer for API.");
