8a2b5e16d951ad81e733af1ba4b308bfa58af506
SkunkService <skunkplatformer@gmail.com>
2024-08-31 19:55:40 -0300

    Create index.js

---
+++ docs/discord/index.js
@@ -0,0 +1,8 @@
+// Get the element by its ID
+const joinButton = document.getElementById("join");
+
+// Add an event listener to handle the click
+joinButton.addEventListener("click", function() {
+    // Navigate to the URL
+    window.location.href = "https://discord.gg/pZNkDhYZtK";
+});
