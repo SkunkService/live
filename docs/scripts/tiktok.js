ee387dae86388da362e3296ef48da805cb057475
SkunkService <skunkplatformer@gmail.com>
2024-09-02 05:49:46 -0300

    Update tiktok.js

--- docs/scripts/tiktok.js
+++ docs/scripts/tiktok.js
@@ -1,17 +1,22 @@
 console.log("[WARN]: Could not be working for Stream List!\n[WARN]: Will not be longer for API.");
 
-// Añadir evento al botón de aceptar para ocultar la alerta
 document.getElementById('accept').addEventListener('click', () => {
-    const alertBox = document.getElementById('alert');
-    alertBox.style.display = 'none'; // Oculta la alerta
     function showAlert(title, message) {
+        const alertBox = document.getElementById('alert');
+        const blackBg = document.getElementById('black-bg');
         const alertTitle = document.getElementById('alert-title');
         const alertBody = document.getElementById('alert-body');
-        const alertBox = document.getElementById('alert');
 
         alertTitle.textContent = title;
         alertBody.textContent = message;
+
+        blackBg.style.display = 'flex';
         alertBox.style.display = 'block';
+
+        document.getElementById('accept').addEventListener('click', () => {
+            blackBg.style.display = 'none';
+            alertBox.style.display = 'none';
+        };
     }
 
     showAlert('Warning Pop-Out', 'You need to Block that the Pop-Out might be taken for null using in URL: https://skunkservice.github.io/live/null.');
