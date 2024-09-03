e88fb91809e839d3d6ec6692e8e44594d1df7c2d
SkunkService <skunkplatformer@gmail.com>
2024-09-03 03:00:28 -0300

    Update index.js

--- docs/discord/index.js
+++ docs/discord/index.js
@@ -4,5 +4,5 @@ const joinButton = document.getElementById("join");
 // Add an event listener to handle the click
 joinButton.addEventListener("click", function() {
     // Navigate to the URL
-    window.location.href = "https://discord.gg/pZNkDhYZtK";
+    window.location.href = "https://discord.gg/jEtZ2kxmJq";
 });
